dcd3eb372a968a3f4942860eea409d011fd8b6e0
Julian Vara <jvara6282@gmail.com>
2025-10-21 22:26:47 -0700

    Added Google sign up through firebase. Also added imports for SignupForm.tsx. Need to create functions and useAuthState

---
+++ src/components/auth/SignupForm.tsx
@@ -0,0 +1,6 @@
+// Importing necessary Firebase modules 
+// Since the files are in the same project, we can import from '@/firebase/firebase'
+
+// Correct path for the import so ../../ auth -> components -> firebase/firebase.ts 
+import { auth, db } from "../../firebase/firebase"; 
+
