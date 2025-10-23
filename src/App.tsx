93d337bb25df46b9f78380c82c6a4e862214e894
Vincuitsina <floydboys91213@gmail.com>
2025-10-23 04:32:38 +0000

    Made StockChart Test File

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Box, Typography } from '@mui/material';
 import Footer from "./components/layout/Footer";
 import Hero from './components/layout/Hero';
 
-
 function App() {
   return (
     <>
