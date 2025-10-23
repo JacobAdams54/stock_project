81843189b93cb985d08a2f03691fbdca9195ca13
HumaneVessel <ja_otero@icloud.com>
2025-10-22 21:52:43 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography } from '@mui/material';
 import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
+import Hero from "./components/layout/Hero";
+
 
 function App() {
   return (
@@ -32,3 +33,4 @@ function App() {
 }
 
 export default App;
+
